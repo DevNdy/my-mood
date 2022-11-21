8cc388bf4c494dcd3e773fd747fc64a829614a8d
DevNdy <developpeur.ndy@gmail.com>
2022-11-21 16:28:42 +0100

    F03 feat HOME > Create navbar

---
+++ src/components/navbar/NavBar.tsx
@@ -0,0 +1,58 @@
+import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import { AuthContext } from "../../context/AuthContext";
+import { auth } from "../../firebase-config";
+import { themeColors } from "../../theme/theme";
+
+const NavBar = () => {
+  const navigate = useNavigate();
+  const { currentUser } = useContext(AuthContext);
+
+  function handleLogOut() {
+    try {
+      auth.signOut();
+      navigate("/");
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  return (
+    <NavBarStyled>
+      <p>{currentUser.displayName}</p>
+      <button onClick={handleLogOut}>se déconnecter</button>
+    </NavBarStyled>
+  );
+};
+
+const NavBarStyled = styled.header`
+  padding: 10px 20px 10px 20px;
+  background-color: ${themeColors.primaryColor};
+  border-radius: 12px;
+  position: fixed;
+  top: 20px;
+  left: 20px;
+
+  p {
+    margin: 0;
+    font-size: 22px;
+    font-weight: bold;
+    margin-left: 5px;
+    text-transform: capitalize;
+  }
+
+  button {
+    border: none;
+    background-color: ${themeColors.fond};
+    border-radius: 30px;
+    padding: 3px 10px 3px 10px;
+    cursor: pointer;
+    opacity: 0.6;
+    &:hover {
+      opacity: 1;
+    }
+  }
+`;
+
+export default NavBar;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,23 +1,11 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
-import { auth } from "../firebase-config";
+import NavBar from "../components/navbar/NavBar";
 
 const HomePage = () => {
-  const navigate = useNavigate();
-
-  function handleLogOut() {
-    try {
-      auth.signOut();
-      navigate("/");
-    } catch (err) {
-      console.log(err);
-    }
-  }
-
   return (
     <HomePageStyled>
-      <button onClick={handleLogOut}>deco</button>
+      <NavBar />
     </HomePageStyled>
   );
 };
