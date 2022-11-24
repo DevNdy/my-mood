115b1f3f8daf0620f17c700266e0503147ce5fea
DevNdy <developpeur.ndy@gmail.com>
2022-11-24 10:37:43 +0100

    F05 feat MOOD > Create right side

---
+++ src/components/listMood/rightSideMood/RightSideMood.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const RightSideMood = () => {
+  return <RightSideMoodStyled>hello</RightSideMoodStyled>;
+};
+
+const RightSideMoodStyled = styled.div`
+  height: 100vh;
+  width: 100vw;
+`;
+
+export default RightSideMood;
